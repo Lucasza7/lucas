c3bb3e724caf2538afe28512c2eba0900d859f8b
Lucasza7 <133224905+Lucasza7@users.noreply.github.com>
2023-05-11 14:33:16 +0200

    Add files via upload

---
+++ week1.php
@@ -0,0 +1,26 @@
+<?php
+  $Getal1 = 12;
+  $Getal2 = 10;
+  $resultaat = $Getal1 + $Getal2;
+  echo $resultaat;
+?>
+
+<?php
+  $Getal1 = 12;
+  $Getal2 = 10;
+  $resultaat = $Getal1 * $Getal2;
+  echo $resultaat;
+?>
+
+<?php
+$naam = "Lucas";
+echo "Welkom " . $naam . "!";
+?>
+
+<?php
+
+$mijnNaam = "Carl";
+$jouwNaam = "Camel"; 
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+?>
